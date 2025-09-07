da1944320dea6a96c6de34d99a187c9b93ebf2a8
pro660 <gw904202@gmail.com>
2025-09-07 22:52:09 +0900

    페이지 구현중(api연동 전)

---
+++ src/jsx/loginPage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "../css/loginPage.css";
+
+/* SVG 파일을 URL로 사용 (안정적) */
+import heartSvg from "../image/loginPage/heart.svg";
+import logoSvg from "../image/loginPage/logo.svg";
+
+export default function LoginPage({ onKakaoStart }) {
+  const handleKakao = () => onKakaoStart?.();
+
+  return (
+    <main className="login-root" role="main">
+      <section className="arch-card" aria-label="너랑 나랑 소개 및 로그인">
+        {/* 상단 브랜드(하트 + 로고) */}
+        <div className="brand">
+          <img src={heartSvg} alt="" className="heart-img" aria-hidden="true" />
+          <img src={logoSvg} alt="너랑 나랑" className="logo-img" />
+        </div>
+
+        {/* 카피 */}
+        <div className="copy">
+          <p className="headline">
+            평범한 축제가 <span className="em">특별</span>해지는 순간!
+          </p>
+          <p className="sub">
+            당신의 옆자리를 채울 <span className="em-strong">한 사람</span>을 찾아보세요.
+          </p>
+        </div>
+
+        {/* CTA */}
+        <div className="cta">
+          <p className="hint">간편 로그인하고 바로 시작해보세요</p>
+          <button type="button" className="kakao-btn" onClick={handleKakao}>
+            <svg className="kakao-icon" viewBox="0 0 24 24" aria-hidden="true">
+              <path d="M12 3C6.48 3 2 6.5 2 10.82c0 2.7 1.78 5.05 4.46 6.39-.15.54-.55 1.95-.63 2.27-.1.4.15.4.31.29.12-.08 2-1.36 2.82-1.91 1 .14 2.03.21 3.04.21 5.52 0 10-3.5 10-7.85C22 6.5 17.52 3 12 3z" />
+            </svg>
+            카카오로 시작하기
+          </button>
+        </div>
+      </section>
+    </main>
+  );
+}
