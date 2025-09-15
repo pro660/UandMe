ebb9b7684846ec3db5410e1b57a62044c354d96a
최지인 <genie1019002@gmail.com>
2025-09-15 22:11:16 +0900

    wow

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppRouter from './Router';   // ✅ App이 아니라 AppRouter로 변경
+import App from './App';   // ✅ App이 아니라 AppRouter로 변경
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppRouter />
+    <App />
   </React.StrictMode>
 );
 
