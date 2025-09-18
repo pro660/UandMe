00a23095dafb610d6df32b0ae5bb635b0c2733f8
pro660 <gw904202@gmail.com>
2025-09-18 22:06:49 +0900

    MatchingCardPage에 master브랜치 rebase완

---
+++ src/jsx/matching/Card.jsx
@@ -0,0 +1,12 @@
+import "../../css/matching/Card.css";
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card-page">
+        
+    </div>
+  );
+}
+
+export default Card;
