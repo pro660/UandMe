9f3a00227f91b8a365ed396489bca5ab4118a1d7
최지인 <genie1019002@gmail.com>
2025-09-19 10:56:13 +0900

    홈 보호된 라우트

--- src/Router.jsx
+++ src/Router.jsx
@@ -48,9 +48,9 @@ function AppRouter() {
           <Route
             path="/"
             element={
-              // <ProtectedRoute>
+              <ProtectedRoute>
               <Home />
-              /* </ProtectedRoute> */
+              </ProtectedRoute>
             }
           />
           <Route path="/chat" element={<ChatList />} />
